40ddc5b5afc67e79f07a28f537bc3450c8805a27
yiguanxianyu <yiguanxianyu@gmail.com>
2023-04-10 19:53:49 +0800

    fix:update tianditu link

--- src/components/map/MapViewOL.vue
+++ src/components/map/MapViewOL.vue
@@ -4,14 +4,14 @@ import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
 import XYZ from 'ol/source/XYZ'
 import 'ol/ol.css'
-import {onMounted} from "vue";
+import { onMounted } from "vue";
 
 let map;
 // 影像图层
 const tileLayer = new TileLayer({
     title: '天地图卫星影像',
     source: new XYZ({
-        url: `http://t4.tianditu.com/DataServer?T=img_w&tk=${import.meta.env.VITE_API_KEY}&x={x}&y={y}&l={z}`,
+        url: "https://t4.tianditu.gov.cn/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=" + import.meta.env.VITE_API_KEY,
         attributions: ['Map data &copy; <a href="https://www.tianditu.gov.cn/">天地图</a>', 'made by yiguanxianyu'],
     })
 })
@@ -19,7 +19,7 @@ const tileLayer = new TileLayer({
 const tileMark = new TileLayer({
     title: '标注图层',
     source: new XYZ({
-        url: `http://t4.tianditu.com/DataServer?T=cia_w&tk=${import.meta.env.VITE_API_KEY}&x={x}&y={y}&l={z}`,
+        url: "https://t4.tianditu.gov.cn/DataServer?T=cia_w&x={x}&y={y}&l={z}&tk=" + import.meta.env.VITE_API_KEY,
     })
 })
 
@@ -42,5 +42,4 @@ onMounted(() => {
     <div id="map" class="map-home"></div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
